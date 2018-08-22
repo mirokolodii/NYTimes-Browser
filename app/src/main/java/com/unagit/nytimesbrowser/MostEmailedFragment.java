2bd85ed6843b5792e1902ab9a7ad8c2ad500f21a
Myroslav Kolodii <mkolodii@gmail.com>
2018-08-22 11:24:17 +0200

    Fix issue with ViewPager hidden behind tabs in activity_main layout.

--- app/src/main/java/com/unagit/nytimesbrowser/MostEmailedFragment.java
+++ app/src/main/java/com/unagit/nytimesbrowser/MostEmailedFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +16,9 @@ import java.util.ArrayList;
 
 public class MostEmailedFragment extends Fragment {
 
+    private final String LOG_TAG = this.getClass().getName();
     private Context context;
+
     public MostEmailedFragment() {
         // Required empty public constructor
     }
@@ -40,10 +43,27 @@ public class MostEmailedFragment extends Fragment {
     }
 
     private void listArticles(View view) {
-        String[] testStringArray = {"title1", "title2", "title3"};
+//        String[] testStringArray = {"title1", "title2", "title3"};
+//        ArrayList<String> testStringArray = new ArrayList<>();
+//        testStringArray.add("title1");
+//        testStringArray.add("title2");
+//        testStringArray.add("title3");
+        String[] testStringArray = new String[] {
+                "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+        Log.d(LOG_TAG, testStringArray.toString());
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this.context,
                 android.R.layout.simple_list_item_1, testStringArray);
         ListView listView = view.findViewById(R.id.most_emailed_list_view);
         listView.setAdapter(adapter);
+//        testStringArray.add("title4");
     }
 }
