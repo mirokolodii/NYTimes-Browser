905b04ff72df77b5e6fbd941e1bfb196b0388b6a
a264889 <mkolodii@gmail.com>
2018-08-28 16:33:36 +0200

    Replace favorites icons with new versions.

--- app/src/main/java/com/unagit/nytimesbrowser/data/ArticleDao.java
+++ app/src/main/java/com/unagit/nytimesbrowser/data/ArticleDao.java
@@ -3,6 +3,7 @@ package com.unagit.nytimesbrowser.data;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import com.unagit.nytimesbrowser.models.Article;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @Dao
 public interface ArticleDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Article article);
 
     @Delete
