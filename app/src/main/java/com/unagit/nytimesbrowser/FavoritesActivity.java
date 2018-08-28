0044eb2f5a6cbcdd57a4945677e415cee6a1254e
a264889 <mkolodii@gmail.com>
2018-08-28 11:29:12 +0200

    Add Toolbar in FavoritesActivity.

--- app/src/main/java/com/unagit/nytimesbrowser/FavoritesActivity.java
+++ app/src/main/java/com/unagit/nytimesbrowser/FavoritesActivity.java
@@ -2,6 +2,8 @@ package com.unagit.nytimesbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 public class FavoritesActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class FavoritesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Add back button to activity on toolbar
+        try {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            Log.e(this.getClass().getSimpleName(), e.getMessage());
+        }
+
     }
 }
