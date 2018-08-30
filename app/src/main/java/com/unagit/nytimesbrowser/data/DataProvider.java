5e5f0a6189f768206c10d04031bc384ae99650c5
a264889 <mkolodii@gmail.com>
2018-08-30 18:06:58 +0200

    Create separate class as CallbackReceiver of Retrofit call in DataProvider.

--- app/src/main/java/com/unagit/nytimesbrowser/data/DataProvider.java
+++ app/src/main/java/com/unagit/nytimesbrowser/data/DataProvider.java
@@ -22,14 +22,14 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class DataProvider implements Callback<DataWrapper> {
+public class DataProvider {
 
     private static LocalDatabase mDB;
     private Application mApplication;
     private Call<DataWrapper> mCall;
     private NYTAPIService mNytapiService;
 
-    private MutableLiveData<List<Article>> mArticles = new MutableLiveData<>();
+//    private MutableLiveData<List<Article>> mArticles = new MutableLiveData<>();
     private MutableLiveData<List<Article>> mMostViewed = new MutableLiveData<>();
     private MutableLiveData<List<Article>> mMostEmailed = new MutableLiveData<>();
     private MutableLiveData<List<Article>> mMostShared = new MutableLiveData<>();
@@ -49,16 +49,22 @@ public class DataProvider implements Callback<DataWrapper> {
 //        mCall = mNytapiService.getMostEmailed(Constants.Retrofit.NYTApiKey);
 //        Log.d(this.getClass().getSimpleName(), mCall.request().url().toString());
 //        mCall.enqueue(this);
-        return mArticles;
+        CallbackReceiver callbackReceiver = new CallbackReceiver(Constants.Tabs.MOST_EMAILED_TAB);
+        mCall.enqueue(callbackReceiver);
+        return mMostEmailed;
+//        return mArticles;
 //        return mMostEmailed;
     }
 
     public LiveData<List<Article>> getMostShared() {
 //        mCall = mNytapiService.getMostShared(Constants.Retrofit.NYTApiKey);
-//        Log.d(this.getClass().getSimpleName(), mCall.request().url().toString());
+////        Log.d(this.getClass().getSimpleName(), mCall.request().url().toString());
 //        mCall.enqueue(this);
-        return mArticles;
-//        return mMostShared;
+////        return mArticles;
+        CallbackReceiver callbackReceiver = new CallbackReceiver(Constants.Tabs.MOST_SHARED_TAB);
+        mCall.enqueue(callbackReceiver);
+//        return mMostViewed;
+        return mMostShared;
     }
 
     public LiveData<List<Article>> getMostViewed() {
@@ -66,12 +72,16 @@ public class DataProvider implements Callback<DataWrapper> {
         Log.d(this.getClass().getSimpleName(), mCall.request().url().toString());
 //        mCall.enqueue(this);
 //        return mArticles;
-        MostViewedReceiver mostViewedReceiver = new MostViewedReceiver();
-        mCall.enqueue(mostViewedReceiver);
+        CallbackReceiver callbackReceiver = new CallbackReceiver(Constants.Tabs.MOST_VIEWED_TAB);
+        mCall.enqueue(callbackReceiver);
         return mMostViewed;
     }
 
-    private class MostViewedReceiver implements Callback<DataWrapper> {
+    private class CallbackReceiver implements Callback<DataWrapper> {
+        private int mQueryType;
+        CallbackReceiver(int queryType) {
+            mQueryType = queryType;
+        }
 
         @Override
         public void onResponse(Call<DataWrapper> call, Response<DataWrapper> response) {
@@ -82,7 +92,18 @@ public class DataProvider implements Callback<DataWrapper> {
                     for(Article article : articles) {
                         article.generateId();
                     }
-                    DataProvider.this.mMostViewed.setValue(articles);
+                    switch(mQueryType) {
+                        case Constants.Tabs.MOST_EMAILED_TAB:
+                            DataProvider.this.mMostEmailed.setValue(articles);
+                            break;
+                        case Constants.Tabs.MOST_VIEWED_TAB:
+                            DataProvider.this.mMostViewed.setValue(articles);
+                            break;
+                        case Constants.Tabs.MOST_SHARED_TAB:
+                            DataProvider.this.mMostShared.setValue(articles);
+                            break;
+                    }
+
                 } else {
                     DataProvider.this.showErrorToast();
                 }
@@ -94,39 +115,41 @@ public class DataProvider implements Callback<DataWrapper> {
 
         @Override
         public void onFailure(Call<DataWrapper> call, Throwable t) {
-
-        }
-    }
-
-
-    @Override
-    public void onResponse(Call<DataWrapper> call, Response<DataWrapper> response) {
-        Log.d(this.getClass().getSimpleName(), "onResponse is triggered");
-        if (response.isSuccessful()) {
-            DataWrapper dataWrapper = response.body();
-            if (dataWrapper != null) {
-                List<Article> articles = dataWrapper.getArticles();
-                for(Article article : articles) {
-                    article.generateId();
-                    Log.d(DataProvider.this.getClass().getSimpleName(), article.getUrl());
-                    Log.d(DataProvider.this.getClass().getSimpleName(), "ID: " + article.getId());
-                }
-                mArticles.setValue(articles);
-            } else {
-                showErrorToast();
-            }
-        } else {
-            System.out.println(response.errorBody());
+            Log.d(this.getClass().getSimpleName(), "onFailure is triggered");
+            t.printStackTrace();
             showErrorToast();
         }
     }
 
-    @Override
-    public void onFailure(Call<DataWrapper> call, Throwable t) {
-        Log.d(this.getClass().getSimpleName(), "onFailure is triggered");
-        t.printStackTrace();
-        showErrorToast();
-    }
+
+//    @Override
+//    public void onResponse(Call<DataWrapper> call, Response<DataWrapper> response) {
+//        Log.d(this.getClass().getSimpleName(), "onResponse is triggered");
+//        if (response.isSuccessful()) {
+//            DataWrapper dataWrapper = response.body();
+//            if (dataWrapper != null) {
+//                List<Article> articles = dataWrapper.getArticles();
+//                for(Article article : articles) {
+//                    article.generateId();
+//                    Log.d(DataProvider.this.getClass().getSimpleName(), article.getUrl());
+//                    Log.d(DataProvider.this.getClass().getSimpleName(), "ID: " + article.getId());
+//                }
+//                mArticles.setValue(articles);
+//            } else {
+//                showErrorToast();
+//            }
+//        } else {
+//            System.out.println(response.errorBody());
+//            showErrorToast();
+//        }
+//    }
+//
+//    @Override
+//    public void onFailure(Call<DataWrapper> call, Throwable t) {
+//        Log.d(this.getClass().getSimpleName(), "onFailure is triggered");
+//        t.printStackTrace();
+//        showErrorToast();
+//    }
 
     //TODO: design a better solution to separate UI from data providers.
     private void showErrorToast() {
